bd5c6776724bc1e8460d82df14029d85aefa164d
Alivier Zapata <alejandroalivier20@gmail.com>
2022-04-20 20:35:44 -0400

    final

--- src/componentes/proyectos/img-proyectos/imgProyectos.js
+++ src/componentes/proyectos/img-proyectos/imgProyectos.js
@@ -1,10 +1,10 @@
 import './imgproyectos.css'
 
-const ImgProyectos = ({title, link}) =>{
+const ImgProyectos = ({title, link, url}) =>{
     return(
         <div >
             <div className='cont-img'>
-                <img src={link} alt="img" className='img'/>
+                <a href={url} target='_blank'  rel="noreferrer"><img src={link} alt="img" className='img'/></a>
             </div>
             <h3 className='title-img'>{title}</h3>
         </div>
--- src/componentes/proyectos/proyectos.js
+++ src/componentes/proyectos/proyectos.js
@@ -4,8 +4,11 @@ import './proyectos.css'
 
 const Proyectos = () => {
     
-    const links = [{title: 'Quotes Generator', link: 'https://i.imgur.com/YAGEDIC.png'}, {title: 'Insta Pets', link: 'https://i.imgur.com/3Ot0Sm8.png'},{title: 'Gamer Store', link: 'https://i.imgur.com/305xCyQ.png'},{title: 'App Cloud', link: 'https://i.imgur.com/MqpSLrZ.png'}]
-    const list = links.map((item) => <ImgProyectos  title={item.title} link={item.link} key={item.title}/>)
+    const links = [{title: 'Quotes Generator', link: 'https://i.imgur.com/YAGEDIC.png', url: 'https://quotes-generator-alivier.netlify.app/'}, 
+    {title: 'Insta Pets', link: 'https://i.imgur.com/3Ot0Sm8.png', url: 'https://insta-clon-alivier.netlify.app/'},
+    {title: 'Gamer Store', link: 'https://i.imgur.com/305xCyQ.png', url: 'https://alivier-jode-mucho.netlify.app/'},
+    {title: 'App Cloud', link: 'https://i.imgur.com/MqpSLrZ.png', url: 'https://appclimaalivier.netlify.app/'}]
+    const list = links.map((item) => <ImgProyectos  title={item.title} link={item.link} key={item.title} url={item.url}/>)
     
     //<ImgProyectos link={links} title={title[0]}/>
     return(
